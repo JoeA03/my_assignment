be66dbaaec55d68261ff5243fd18d2cfa574275f
EC2 Default User <ec2-user@ip-172-31-41-188.ec2.internal>
2020-04-20 12:09:40 +0000

    new python file added

---
+++ password.py
@@ -0,0 +1,6 @@
+name_input = input('Write your name please: ').title().strip()
+your_name = 'Joe'
+if name_input == your_name:
+print('Hello, ' + your_name + '! The password is : @12')
+else:
+print('Hello, ' + name_input + '! See you later.')
